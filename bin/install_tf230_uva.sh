9265bec8a30df06067db3a3cb2c6d39cc2c59066
vinhtknguyen <ntkimvinh7@gmail.com>
2022-05-21 03:00:59 -0400

    Update install_tf230_uva.sh

--- bin/install_tf230_uva.sh
+++ bin/install_tf230_uva.sh
@@ -1,6 +1,6 @@
 source /data/nguyenvt2/libs/miniconda3/etc/profile.d/conda.sh
-conda create -n tf230_uva python=3.7 -y
-conda activate tf230_uva
+conda create -n tf_uva python=3.7 -y
+conda activate tf_uva
 conda install cudatoolkit=10.1 -y
 conda install cudnn=7.6.5 -y
 yes | pip install tensorflow-gpu==2.3.0
